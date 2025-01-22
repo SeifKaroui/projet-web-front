1991e0d4a66a258fb5a621a59e9e7707e2931fe8
Khalil SD <khalilsd2002@gmail.com>
2025-01-22 03:45:08 +0100

    add delete course functionality for teachers; fix search bar

--- src/app/pages/apps/courses/services/course.service.ts
+++ src/app/pages/apps/courses/services/course.service.ts
@@ -7,19 +7,28 @@ import { Course } from '../models/course.model';
   providedIn: 'root',
 })
 export class CourseService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'http://localhost:3000'; // URL de base de votre API
 
   constructor(private http: HttpClient) {}
 
+  // Récupérer les cours auxquels l'étudiant est inscrit
   getEnrolledCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(`${this.apiUrl}/courses/my-enrolled-courses`);
   }
 
+  // Récupérer les cours créés par l'enseignant
   getMyCourses(): Observable<Course[]> {
     return this.http.get<Course[]>(`${this.apiUrl}/courses/my-courses`);
   }
 
+  // Récupérer un cours par son ID
   getCourseById(id: number): Observable<Course> {
-    return this.http.get<Course>(`${this.apiUrl}/${id}`);
+    return this.http.get<Course>(`${this.apiUrl}/courses/${id}`);
+  }
+
+  // Supprimer un cours par son ID
+  deleteCourse(courseId: number): Observable<void> {
+    const url = `${this.apiUrl}/courses/${courseId}`; 
+    return this.http.delete<void>(url); 
   }
 }
